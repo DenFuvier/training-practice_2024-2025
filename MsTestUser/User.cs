14182b5f48d6fc3dbe15629dfe09fbddf359b6f6
Den Fuvier <Findalone@protonmail.com>
2024-09-24 23:51:01 +0300

    D

---
+++ MsTestUser/User.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace Users
+{
+    public class User:IComparer
+    {
+        public string Login { get; set; }
+
+        public int Password { get; set; }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        int IComparer.Compare(object a, object b)
+        {
+            User c1 = (User)a;
+            User c2 = (User)b;
+            if (c1.Password == c2.Password)
+            {
+                return 0;
+            }
+            else if (c1.Password < c2.Password)
+            {
+                return -1;
+            }
+            return 1;
+
+        }
+    }
+}
---
+++ MsTestUser/UserManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace Users
+{
+    public class UserManager
+    {
+
+        public void Add(User u)
+        {
+
+        }
+
+        public bool Write(List<User> users)
+        {
+
+            string cs = @"server=localhost;userid=DenFuvier;password=N1PGKt1mT3UAlRRa;database=boyk";
+            try
+            {
+                using (var con = new MySqlConnection(cs))
+                {
+                    con.Open();
+                    for (int i = 0; i < users.Count; i++)
+                    {
+                        User user = users[i];
+                        string login = user.Login;
+                        string name = user.Name; ;
+                        string surname = user.Surname;
+                        int password = user.Password;
+
+
+
+
+                        var stm = String.Format("INSERT INTO users (login, name, surname, password) VALUES ('{0}', '{1}', '{2}', '{3}')",
+                                                 login, name, surname, password);
+                        using (var cmd = new MySqlCommand(stm, con))
+                        {
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    con.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        public List<User> ReadAll()
+        {
+            List<User> Users = new List<User>();
+
+            string cs = @"server=localhost;userid=DenFuvier;password=N1PGKt1mT3UAlRRa;database=boyk";
+            try
+            {
+
+                var con = new MySqlConnection(cs);
+
+                con.Open();
+
+                var stm = String.Format("SELECT Name, Surname , password , login FROM users");
+
+                var cmd = new MySqlCommand(stm, con);
+                MySqlDataReader Reader = cmd.ExecuteReader();
+                while (Reader.Read())
+                {
+
+                    string name = Reader.GetString(0);
+                    string surname = Reader.GetString(1);
+                    int password = Reader.GetInt32(2);
+                    string login = Reader.GetString(3);
+                    Users.Add(new User { Name = name, Surname = surname, Login = login, Password = password });
+
+                }
+
+            }
+            catch (Exception Exept)
+            {
+
+
+                Console.WriteLine(Exept.Message);
+                return new List<User>();
+
+            }
+            return Users;
+
+        }
+    }
+}
