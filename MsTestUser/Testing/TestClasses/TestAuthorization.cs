38099870b884aaee18983ed5cd6b617a451097ff
a.boikov <alex8oikov@yandex.ru>
2024-10-08 12:39:59 +0300

    убрал обработанные ревью

--- MsTestUser/Testing/TestClasses/TestAuthorization.cs
+++ MsTestUser/Testing/TestClasses/TestAuthorization.cs
@@ -16,15 +16,13 @@ public class TestAuthorization
     [DataRow("Boyk", "987654", true)]
     [DataRow("ArictT", "boller", false)]
     [DataRow("Pabel", "ModedGun", false)]
-    [DataRow("DenF", "wrongpass", false)]  // Верный логин, но неправильный пароль
-    /// REVIEW. a.boikov. 2024/10/08. Не хватает кейса, где указан верный логин, но неправильный пароль -- done 
+    [DataRow("DenF", "wrongpass", false)]
     [TestMethod]
     public void TestUserAuthorization(string Login, string Password, bool expected)
     {
         UserManager userManager = new UserManager();
         userManager.Delete();
         userManager.Write(testUsers_);
-        /// REVIEW. a.boikov. 2024/10/08. Avtorizate -> Authorization .. Следовательно и файл тоже -- done 
         bool Actual = userManager.Authorization(Login, Password);
         Assert.AreEqual(expected, Actual);
 
